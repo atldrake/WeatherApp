cd300c595c28d4b94381abd733ef2d27506327cb
ATL Drake / FirmTech <atldrake532@gmail.com>
2020-08-16 16:21:12 +0000

    Grap API Data with Request - Format API Results - Home View Error Handling

--- lookup/views.py
+++ lookup/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+    api_request = requests.get('http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=F8D7FC56-2B41-4B5E-8110-3B4639ABAC30')
+    
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+       api = 'Error ...'
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
